3ebb648ad842a9acd1f093bba9c8e3333f3d8d58
Danil <busymangorbenko@gmail.com>
2016-02-10 12:55:44 +0200

    added generateApproveToken to rooms_helper

--- app/tests/unit/admin/rooms_helper.js
+++ app/tests/unit/admin/rooms_helper.js
@@ -4,20 +4,35 @@ var expect = require('chai').expect;
 
 module.exports = function (request, config, share) {
   return {
-    createRoom: function (roomName, fieldInShare) {
-      let data = {name: roomName};
+    createRoom: function (options, done) {
+      let data = {name: options.roomName};
 
       request
         .post('/rooms')
         .send(data)
-        .set('Authorization', share.authentication.token)
+        .set('Authorization', options.token)
         .expect(function (res) {
           expect(res.body).to.be.an('object');
           expect(res.body.response).to.be.an('object');
-          expect(res.body.response.name).to.equal(data.name);
+          expect(res.body.response.name).to.equal(options.roomName);
         })
         .expect(function (res) {
-          share.rooms[fieldInShare] = res.body.response.id;
+          share.rooms[options.field] = res.body.response;
+        })
+        .expect(200, done);
+    },
+    generateApproveToken: function (options, done) {
+      let roomId = options.roomId;
+
+      request
+        .get(`/rooms/${roomId}/approve`)
+        .set('Authorization', options.token)
+        .expect(function (res) {
+          expect(res.body).to.be.an('object');
+          expect(res.body.response).to.be.an('string');
+        })
+        .expect(function (res) {
+          share.rooms[options.field] = res.body.response;
         })
         .expect(200, done);
     }
