13b7d042be701ea99511226bcf8b5801b9a0b026
Danil <busymangorbenko@gmail.com>
2016-02-10 13:31:31 +0200

    added docs for rooms_helper

--- app/tests/unit/admin/rooms_helper.js
+++ app/tests/unit/admin/rooms_helper.js
@@ -3,25 +3,50 @@
 var expect = require('chai').expect;
 
 module.exports = function (request, config, share) {
-  return {
-    createRoom: function (options, done) {
-      let data = {name: options.roomName};
+  var helper = {};
 
-      request
-        .post('/rooms')
-        .send(data)
-        .set('Authorization', options.token)
-        .expect(function (res) {
-          expect(res.body).to.be.an('object');
-          expect(res.body.response).to.be.an('object');
-          expect(res.body.response.name).to.equal(options.roomName);
-        })
-        .expect(function (res) {
-          share.rooms[options.field] = res.body.response;
-        })
-        .expect(200, done);
-    },
-    generateApproveToken: function (options, done) {
+  /**
+   * Creates room by specified data and assigns response to share object
+   * in specified field.
+   *
+   * @param {Object} options
+   * @param {String} options.roomName Name of the room to create
+   * @param {String} options.token User authorization token
+   * @param {String} options.field Name of field to assign to share object
+   *
+   * @param done Callback
+   */
+
+  helper.createRoom = function (options, done) {
+    let data = {name: options.roomName};
+
+    request
+      .post('/rooms')
+      .send(data)
+      .set('Authorization', options.token)
+      .expect(function (res) {
+        expect(res.body).to.be.an('object');
+        expect(res.body.response).to.be.an('object');
+        expect(res.body.response.name).to.equal(options.roomName);
+      })
+      .expect(function (res) {
+        share.rooms[options.field] = res.body.response;
+      })
+      .expect(200, done);
+  };
+
+  /**
+   * Generates approve token
+   *
+   * @param {Object} options
+   * @param {String|Number} options.roomId ID of the room to get link for.
+   * @param {String} options.token User authorization token
+   * @param {String} options.field Name of field to assign to share object
+
+   * @param done Callback
+   */
+
+  helper.generateApproveToken = function (options, done) {
       let roomId = options.roomId;
 
       request
@@ -35,6 +60,5 @@ module.exports = function (request, config, share) {
           share.rooms[options.field] = res.body.response;
         })
         .expect(200, done);
-    }
-  };
+    };
 };
